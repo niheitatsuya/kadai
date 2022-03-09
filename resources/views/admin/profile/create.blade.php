876926db55e2365551a05de217c019fe1f3ea79a
nihei-tatsuya <n.tatsuya2727@gmail.com>
2022-03-09 06:52:45 +0000

    kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -6,6 +6,34 @@
     <div class="row">
         <div class="col-md-8 mx-auto">
             <h2>my プロフィール</h2>
+            <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+                
+                <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    
+            
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,11 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create', 'Admin/ProfileController@create')->middleware('auth');
+    Route::post('profile/edit', 'Admin/ProfileController@update')->middleware('auth');
+    
 });
 Auth::routes();
 
